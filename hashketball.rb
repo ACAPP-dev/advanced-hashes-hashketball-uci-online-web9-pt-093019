f37c45b0818245ce0663b5c2665dc38819b30813
Andrew Capp <acapp909@gmail.com>
2019-09-30 04:01:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -224,7 +224,7 @@ def big_shoe_rebounds()
          # binding.pry
          player_shoe = {player[:player_name] => [player[:shoe], player[:rebounds]]}
          binding.pry
-         player_shoe.select |player, value|
+         #player_shoe.select |player, value|
          
         end
       end
